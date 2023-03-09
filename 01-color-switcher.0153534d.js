e07f5a8a467e348e6c70d6d280642f79183e52b0
ZachepaRostislav <66321815+ZachepaRostislav@users.noreply.github.com>
2023-03-09 19:50:29 +0000

    Deploying to gh-pages from @ ZachepaRostislav/goit-js-hw-09@bac27878ae657e480c8cd37726db9796d7701aef 🚀

---
+++ 01-color-switcher.0153534d.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")},e=null;t.startBtn.addEventListener("click",(function(n){e=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),n.target.setAttribute("disabled","disabled")})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.0153534d.js.map
