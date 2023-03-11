ab9dda2b03ccabb91e97c761c72a9325e503ad00
ZachepaRostislav <zachepa.rostislav@gmail.com>
2023-03-11 13:19:20 +0200

    third

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+}
+
+refs.form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  for (let i = 0; i < refs.amount.value; i++) {
+    let position = i + 1
+    const delay = Number(refs.delay.value) + refs.step.value * i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  event.target.reset()
+})
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay })
+      } else {
+        // Reject
+        reject({ position, delay })
+      }
+    }, delay);
+  })
 }
+
+
+
+
